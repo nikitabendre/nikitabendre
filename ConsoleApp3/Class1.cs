803de488d4cdf8e15a0b7a9cdb13310910224549
nikita.bendre <nikita.bendre@AZ-VDI-D1-231>
2022-05-20 06:09:29 -0700

    Add project files.

---
+++ ConsoleApp3/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public class program
+    {
+        public static void main(string[] args)
+        {
+            double Result, x, y;
+
+            x = 3;
+            y = 4;
+
+            Result= Math.Pow(x, y);
+            Console.WriteLine(Result);
+
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+namespace ConsoleApp3
+{
+    internal class Class1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Collection List");
+            // Creating arraylist
+            ArrayList mylist = new ArrayList();
+            // Adding Elements
+            mylist.Add(2);
+            mylist.Add(3);
+            mylist.Add(4);
+
+            mylist.Add(10.10);
+            mylist.Add(20.30);
+            mylist.Add(30.40);
+
+            mylist.Add('A');
+            mylist.Add('B');
+            mylist.Add('C');
+
+            mylist.Add("vicky");
+            mylist.Add("abhi");
+            mylist.Add("amit");
+
+            int sum = 0;
+            int count = 0;
+
+            Console.WriteLine($"Length of array list={mylist.Count}");
+            foreach (object obj in mylist)
+            {
+                if (obj is int)
+                {
+                    sum = sum + (int)obj;
+                    if ((int)obj % 2 == 0)
+                    {
+                        Console.WriteLine("Even Number=" + (int)obj);
+                        count++;
+                        Console.WriteLine();
+                    }
+                }
+                else if (obj is string)
+                {
+                    string obj1 = (string)obj;
+                    Console.WriteLine("String Name" +
+                        "=" + (string)obj + ",String Length" +
+                        "=" + obj1.Length + ", Index=" + 
+                        mylist.IndexOf((string)obj));
+                }
+
+
+            }
+            Console.WriteLine($"int sum=" + sum);
+            Console.WriteLine();
+            Console.WriteLine($"Count of even numbers are{count}");
+
+
+
+
+
+        }
+
+    }
+}
+
